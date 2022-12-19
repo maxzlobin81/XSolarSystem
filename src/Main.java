06be571a59d4ae004c3b250ec2dc9c264f5ff4ad
maxzlobin81 <maxzlobin@gmail.com>
2022-12-19 10:07:07 -0600

    Feature sun is added but there errors that need to be resloved

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,15 @@
+import XSolarSystem.src.FeatureSun;
+
 public class Main {
     public static void main(String[] args) {
         SolarSystem xx=new SolarSystem();
         xx.moon=1;
         xx.sun=1;
         xx.planet=2;
+
+        FeatureSun xeron=new FeatureSun();
+        xeron.Heat="1234567";
+        xeron.size=12345;
+        xeron.Temperature="3434";
     }
 }
